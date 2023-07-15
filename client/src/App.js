1d209ab707b10b2f60bf7cb240649607eadf3681
JeongHanO <ojh980228@gmail.com>
2023-07-15 19:30:21 +0900

    ✨ Capture canvas by Export button

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef } from "react";
 import Canvas from "./components/Canvas/Canvas";
 import TopBar from "./components/TopBar/TopBar";
 import ToolBar from "./components/ToolBar/ToolBar";
+import html2canvas from 'html2canvas';
+import fileDownload from 'js-file-download';
 
 function App() {
+  const captureRef = useRef(null);
+
+  const handleExportClick = () => {
+    if (captureRef.current) {
+      html2canvas(captureRef.current).then(canvas => {
+        canvas.toBlob(blob => {
+          fileDownload(blob, 'screenshot.png');
+        });
+      });
+    }
+  }
+
   return (
     <div>
-      <TopBar />
-      <div style={{ display: "grid", gridTemplateColumns: "auto 1fr" }}>
-        <ToolBar />
-        <Canvas />
+      <TopBar onExportClick={handleExportClick} />
+        <div style={{ display: "grid", gridTemplateColumns: "auto 1fr" }}>
+      <ToolBar />
+      <Canvas ref={captureRef} />
       </div>
     </div>
   );
