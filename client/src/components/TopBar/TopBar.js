28c5a2ed3486041e264b3b43c4c04bb5a5a41c24
Hyungjun <dlgudwns1207@gmail.com>
2023-07-16 02:18:32 +0900

    ✨ add GPT API on topbar

--- client/src/components/TopBar/TopBar.js
+++ client/src/components/TopBar/TopBar.js
@@ -1,23 +1,52 @@
-// TopBar.js
-import React from 'react';
-import "./TopBar.css"
+import "./TopBar.css";
+import { useState } from "react";
 
-function TopBar({ onExportClick }) {
-  return (
-    <div className="topbar-background">
-      <div className="code">
-        #CODE
-      </div>
-      <div className="topbar-menu">
-        <div className="member-info">
-          <button>Button 1</button>
+function TopBar() {
+    const [animalInput, setAnimalInput] = useState("");
+    const [result, setResult] = useState();
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        try {
+            const response = await fetch("/api/generate", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ animal: animalInput }),
+            });
+
+            const data = await response.json();
+            if (response.status !== 200) {
+                throw data.error || new Error(`Request failed with status ${response.status}`);
+            }
+
+            setResult(data.result);
+            setAnimalInput("");
+        } catch (error) {
+            // Consider implementing your own error handling logic here
+            console.error(error);
+            alert(error.message);
+        }
+    }
+    return (
+        <div className="topbar-background">
+            <div className="code">#CODE</div>
+            <form onSubmit={onSubmit}>
+                <input type="text" name="GPT" placeholder="Enter value" value={animalInput} onChange={(e) => setAnimalInput(e.target.value)} />
+                <input type="submit" value="submit" />
+            </form>
+            <div>{result}</div>
+            <div className="topbar-menu">
+                <div className="member-info">
+                    <button>Button 1</button>
+                </div>
+                <div className="export-btn">
+                    <button>EXPORT</button>
+                </div>
+            </div>
         </div>
-        <div className="export-btn">
-          <button onClick={onExportClick}>EXPORT</button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default TopBar;
