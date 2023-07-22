8a2f4e7d547854790e0b366853f127e4b010d629
Hyungjun <dlgudwns1207@gmail.com>
2023-07-23 04:19:45 +0900

    ✨ Add Highlight when select object

--- client/src/App.js
+++ client/src/App.js
@@ -2,31 +2,31 @@ import React, { useRef } from "react";
 import Canvas from "./components/Canvas/Canvas";
 import TopBar from "./components/TopBar/TopBar";
 import ToolBar from "./components/ToolBar/ToolBar";
-import html2canvas from 'html2canvas';
-import fileDownload from 'js-file-download';
+import html2canvas from "html2canvas";
+import fileDownload from "js-file-download";
 
 function App() {
-  const captureRef = useRef(null);
+    const captureRef = useRef(null);
 
-  const handleExportClick = () => {
-    if (captureRef.current) {
-      html2canvas(captureRef.current).then(canvas => {
-        canvas.toBlob(blob => {
-          fileDownload(blob, 'screenshot.png');
-        });
-      });
-    }
-  };
+    const handleExportClick = () => {
+        if (captureRef.current) {
+            html2canvas(captureRef.current).then((canvas) => {
+                canvas.toBlob((blob) => {
+                    fileDownload(blob, "screenshot.png");
+                });
+            });
+        }
+    };
 
-  return (
-    <div>
-      <TopBar onExportClick={handleExportClick} />
-        <div style={{ display: "grid", gridTemplateColumns: "auto 1fr" }}>
-      <ToolBar />
-      <Canvas ref={captureRef} />
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <TopBar onExportClick={handleExportClick} />
+            <div style={{ display: "grid", gridTemplateColumns: "auto 1fr" }}>
+                <ToolBar />
+                <Canvas ref={captureRef} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
