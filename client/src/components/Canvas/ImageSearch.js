418d2ef0cec73b7cba0f692796398179dfd83738
Hyungjun <dlgudwns1207@gmail.com>
2023-07-26 02:40:22 +0900

    ♻️ Refactor work

--- client/src/components/Canvas/ImageSearch.js
+++ client/src/components/Canvas/ImageSearch.js
@@ -30,13 +30,6 @@ const styles = {
     },
 };
 
-function srcset(image, size, rows = 1, cols = 1) {
-    return {
-        src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-        srcSet: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format&dpr=2 2x`,
-    };
-}
-
 const ImageSearch = (props) => {
     const [img, setImg] = useState("");
     const [res, setRes] = useState([]);
