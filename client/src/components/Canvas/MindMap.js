c1e4633c26a8d0539ccc077f170b2868eb5707b4
SimSimEEE <smileteeth14@gmail.com>
2023-08-01 23:15:51 +0900

    Revert "🔀Merge pull request #188 from JeongHanO/SimSimEEE/gptUI"
    This reverts commit bbde790d5ed33af03a130ae73fe9aa90cc7ef664, reversing
    changes made to 768f96bd99b3dc23de211bfb78e3b17095415bd1.

--- client/src/components/Canvas/MindMap.js
+++ client/src/components/Canvas/MindMap.js
@@ -223,8 +223,6 @@ const MindMap = ({
     const [showPopup, setShowPopup] = useState(false);
     const [newNodeLabels, setNewNodeLabels] = useState([]);
     const [clickedNodeId, setClickedNodeId] = useState("");
-    const [connectedNodeLabels, setConnectedNodeLabels] = useState([]);
-    const [allNodeLabels, setAllNodeLabels] = useState([]);
 
     const memoizedHandleClickOutside = useCallback(
         handleClickOutside(
@@ -934,8 +932,8 @@ const MindMap = ({
             return;
         }
 
-        setConnectedNodeLabels(getConnectedNodeLabels(clickedNodeId, ymapRef));
-        setAllNodeLabels(getAllNodeLabels(ymapRef));
+        const connectedNodeLabels = getConnectedNodeLabels(clickedNodeId, ymapRef);
+        const allNodeLabels = getAllNodeLabels(ymapRef);
         const newNodeLabels = await fetchNewNodeLabels(connectedNodeLabels, allNodeLabels);
 
         if (newNodeLabels.length === 0) {
@@ -944,18 +942,6 @@ const MindMap = ({
         }
         setShowPopup(true);
         setNewNodeLabels(newNodeLabels);
-    };
-
-    const handleClosePopup = () => {
-        setShowPopup(false);
-    };
-
-    const handleRestart = async () => {
-        setAllNodeLabels(allNodeLabels.concat(newNodeLabels));
-        console.log("allNodeLabels:", allNodeLabels);
-        const addedLabels = await fetchNewNodeLabels(connectedNodeLabels, allNodeLabels);
-
-        setNewNodeLabels(newNodeLabels.concat(addedLabels));
     };
 
     const handleDeleteLabel = (labelToDelete) => {
@@ -1186,8 +1172,6 @@ const MindMap = ({
                     newNodeLabels={newNodeLabels}
                     onDelete={handleDeleteLabel}
                     onCreate={handleCreate}
-                    onClose={handleClosePopup}
-                    onRestart={handleRestart}
                 />
             )}
         </div>
