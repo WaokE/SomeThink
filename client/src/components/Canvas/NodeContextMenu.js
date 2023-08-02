8381ad15536b6a7dd013aad2da341d42d83041cf
SimSimEEE <smileteeth14@gmail.com>
2023-08-02 21:40:35 +0900

    💄ui/ux update
    우클릭 시 ui 개선  경고&알림 토스트창 ui 개선

--- client/src/components/Canvas/NodeContextMenu.js
+++ client/src/components/Canvas/NodeContextMenu.js
@@ -1,4 +1,15 @@
-import "./ContextMenu.css";
+// import "./ContextMenu.css";
+import Divider from "@mui/material/Divider";
+import Paper from "@mui/material/Paper";
+import MenuList from "@mui/material/MenuList";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Typography from "@mui/material/Typography";
+import PsychologyAltOutlinedIcon from "@mui/icons-material/PsychologyAltOutlined";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ShapeLineTwoToneIcon from "@mui/icons-material/ShapeLineTwoTone";
 
 const NodeContextMenu = ({
     selectedNode,
@@ -34,12 +45,38 @@ const NodeContextMenu = ({
     };
 
     return (
-        <ul>
-            <li onClick={handleAddChildNode}>자식 노드 추가</li>
-            <li onClick={handleDeleteNode}>노드 제거</li>
-            <li onClick={handleAddEdge}>엣지 추가</li>
-            <li onClick={handleairecommend}>ai 추천 노드</li>
-        </ul>
+        <Paper sx={{ width: 150, maxWidth: "100%" }}>
+            <MenuList>
+                <MenuItem onClick={handleAddChildNode}>
+                    <ListItemIcon>
+                        <AddCircleIcon fontSize="small" />
+                    </ListItemIcon>
+                    <ListItemText>노드 추가</ListItemText>
+                    <Typography variant="body2" color="text.secondary"></Typography>
+                </MenuItem>
+                <MenuItem onClick={handleDeleteNode}>
+                    <ListItemIcon>
+                        <DeleteIcon fontSize="small" />
+                    </ListItemIcon>
+                    <ListItemText>노드 제거</ListItemText>
+                    <Typography variant="body2" color="text.secondary"></Typography>
+                </MenuItem>
+                <MenuItem onClick={handleAddEdge}>
+                    <ListItemIcon>
+                        <ShapeLineTwoToneIcon fontSize="small" />
+                    </ListItemIcon>
+                    <ListItemText>엣지 추가</ListItemText>
+                    <Typography variant="body2" color="text.secondary"></Typography>
+                </MenuItem>
+                <Divider />
+                <MenuItem onClick={handleairecommend}>
+                    <ListItemIcon>
+                        <PsychologyAltOutlinedIcon fontSize="medium" />
+                    </ListItemIcon>
+                    <ListItemText>추천 키워드</ListItemText>
+                </MenuItem>
+            </MenuList>
+        </Paper>
     );
 };
 
