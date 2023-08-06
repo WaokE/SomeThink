a431312bf6f2194497252b370fd4814b764cc6d5
SimSimEEE <smileteeth14@gmail.com>
2023-08-06 17:21:37 +0900

    ✨add node search UI

--- client/src/components/Canvas/MarkDown.js
+++ client/src/components/Canvas/MarkDown.js
@@ -11,6 +11,52 @@ import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import TreeItem, { useTreeItem } from "@mui/lab/TreeItem";
 import clsx from "clsx";
 
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+        marginLeft: theme.spacing(1),
+        width: "auto",
+    },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: "inherit",
+    "& .MuiInputBase-input": {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create("width"),
+        width: "100%",
+        [theme.breakpoints.up("sm")]: {
+            width: "12ch",
+            "&:focus": {
+                width: "20ch",
+            },
+        },
+    },
+}));
+
 const styles = {
     markdown: {
         width: "22%",
@@ -245,6 +291,15 @@ const GraphToMarkdown = ({
         <Slide direction="left" in={isMarkdownVisible} mountOnEnter unmountOnExit>
             <Box sx={{ ...styles.markdown, ...style }}>
                 <div style={{ display: "flex", justifyContent: "flex-end", paddingRight: "0.5%" }}>
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search..."
+                            inputProps={{ "aria-label": "search" }}
+                        />
+                    </Search>
                     <IconButton onClick={handleMarkdownVisible}>
                         <ArrowBackRoundedIcon sx={styles.closeButton} />
                     </IconButton>
