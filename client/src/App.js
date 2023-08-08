815f96dbc2c235de8ac679a67207d1400aa7b980
huir0 <keronruss@gmail.com>
2023-08-08 17:03:29 +0900

    ✨separate pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import MindMap from "./components/Canvas/MindMap";
 import { OpenVidu } from "openvidu-browser";
 import axios from "axios";
@@ -8,6 +8,10 @@ import LoadingBox from "./components/LoadingScreen/LoadingBox";
 
 import "./App.css";
 import "./Fonts/Font.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/Page/HomePage";
+import SessionPage from "./components/Page/SessionPage";
 
 const APPLICATION_SERVER_URL =
     process.env.NODE_ENV === "production" ? "" : "https://somethink.online/";
@@ -35,12 +39,22 @@ class App extends Component {
         this.handleChangeSessionId = this.handleChangeSessionId.bind(this);
         this.handleChangeUserName = this.handleChangeUserName.bind(this);
         this.handleMainVideoStream = this.handleMainVideoStream.bind(this);
-        this.onbeforeunload = this.onbeforeunload.bind(this);
+        // this.onbeforeunload = this.onbeforeunload.bind(this);
         this.handleCreateSession = this.handleCreateSession.bind(this);
         this.handleJoinSession = this.handleJoinSession.bind(this);
     }
 
     componentDidMount() {
+        const storedSessionId = sessionStorage.getItem("sessionId");
+        const storedUserName = sessionStorage.getItem("userName");
+        if (storedSessionId) {
+            this.setState({ mySessionId: storedSessionId, myUserName: storedUserName }, () => {
+                this.joinSession();
+            });
+        } else {
+            // window.location.href = "/";
+            // this.handleCreateSession();
+        }
         window.addEventListener("beforeunload", this.onbeforeunload);
     }
 
@@ -48,9 +62,9 @@ class App extends Component {
         window.removeEventListener("beforeunload", this.onbeforeunload);
     }
 
-    onbeforeunload(event) {
-        this.leaveSession();
-    }
+    // onbeforeunload(event) {
+    //     this.leaveSession();
+    // }
 
     handleChangeSessionId(e) {
         const sessionId = e.target.value.replace(/#/g, "");
@@ -80,18 +94,20 @@ class App extends Component {
         this.joinSession();
     }
 
-    handleJoinSession() {
+    handleJoinSession(callback) {
         const mySessionId = this.state.mySessionId;
-        if (mySessionId === undefined || mySessionId === "" || mySessionId === "code") {
+        if (mySessionId === undefined || mySessionId === "") {
             alert("존재하지 않는 방입니다.");
-            return;
+            callback(false);
         }
 
         this.validateSessionId(mySessionId).then((response) => {
             if (response === true) {
                 this.joinSession();
+                callback(true);
             } else {
                 alert("존재하지 않는 방입니다.");
+                callback(false);
             }
         });
     }
@@ -231,6 +247,8 @@ class App extends Component {
                                 mainStreamManager: publisher,
                                 publisher: publisher,
                             });
+                            sessionStorage.setItem("sessionId", this.state.mySessionId);
+                            sessionStorage.setItem("userName", this.state.myUserName);
                             this.handleSessionJoin();
                         })
                         .catch((error) => {
@@ -267,7 +285,7 @@ class App extends Component {
                 console.error("Error leaving the session on the server:", error);
             });
 
-        window.location.reload();
+        // window.location.reload();
 
         // Empty all properties...
         this.OV = null;
@@ -299,146 +317,46 @@ class App extends Component {
         const myUserName = this.state.myUserName;
         const audioEnabled = this.state.audioEnabled;
         return (
-            <div className="container">
-                {this.state.session === undefined ? (
-                    <div id="join">
-                        <div className="big-circles" style={{ pointerEvents: "none" }}>
-                            <div className="big-circle"></div>
-                            <div className="big-circle"></div>
-                            <div className="big-circle"></div>
-                        </div>
-                        <section id="home">
-                            <div className="slide-wrapper">
-                                <div className="smallcircles" style={{ pointerEvents: "none" }}>
-                                    <div className="small-circle"></div>
-                                    <div className="small-circle"></div>
-                                    <div className="small-circle"></div>
-                                    <div className="small-circle"></div>
-                                    <div className="small-circle"></div>
-                                    <div className="small-circle"></div>
-                                </div>
-                                <div id="join-dialog" className="jumbotron vertical-center">
-                                    <h1 className="logo"></h1>
-                                    <form
-                                        className="form-group name"
-                                        onSubmit={this.handleCreateSession}
-                                    >
-                                        <p>
-                                            <label> NAME </label>
-                                            <input
-                                                className="form-control"
-                                                type="text"
-                                                id="userName"
-                                                value={myUserName}
-                                                onChange={this.handleChangeUserName}
-                                                required
-                                            />
-                                        </p>
-                                    </form>
-                                    <div id="join-dialog-content">
-                                        <div className="first">
-                                            <p className="create">
-                                                <label id="label-create"> NEW ROOM </label>
-                                                <input
-                                                    onClick={() => {
-                                                        this.setState(
-                                                            {
-                                                                isLoading: true,
-                                                            },
-                                                            () => {
-                                                                this.forceUpdate();
-                                                            }
-                                                        );
-                                                        this.handleCreateSession();
-                                                    }}
-                                                    className="btn btn-lg btn-success create"
-                                                    name="commit"
-                                                    type="submit"
-                                                    value="CREATE"
-                                                />
-                                            </p>
-                                        </div>
-                                        {/* </form> */}
-                                        <div>
-                                            <form
-                                                className="form-group"
-                                                onSubmit={(event) => {
-                                                    event.preventDefault();
-                                                    this.handleJoinSession();
-                                                }}
-                                            >
-                                                <p>
-                                                    {/* <label> 코드 </label> */}
-                                                    <input
-                                                        className="form-control"
-                                                        type="text"
-                                                        id="sessionId"
-                                                        onChange={this.handleChangeSessionId}
-                                                        style={{ pointerEvents: "auto" }}
-                                                        // pattern="[0-9A-Za-z]+"
-                                                        title="영어나 숫자만 입력해주세요"
-                                                        placeholder="   # INVITE CODE"
-                                                    />
-                                                </p>
-                                                <p className="text-center">
-                                                    <input
-                                                        onClick={() => {
-                                                            this.setState(
-                                                                {
-                                                                    isLoading: true,
-                                                                },
-                                                                () => {
-                                                                    this.forceUpdate();
-                                                                }
-                                                            );
-                                                            console.log(mySessionId);
-                                                        }}
-                                                        className="btn btn-lg btn-success"
-                                                        name="commit"
-                                                        type="submit"
-                                                        value="JOIN"
-                                                    />
-                                                </p>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-                    </div>
-                ) : null}
-
-                {this.state.session !== undefined ? (
-                    <div id="session">
-                        <div id="session-header">
-                            <MindMap
-                                sessionId={mySessionId}
+            <Router>
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element={
+                            <HomePage
+                                myUserName={myUserName}
+                                handleChangeUserName={this.handleChangeUserName}
+                                handleCreateSession={this.handleCreateSession}
+                                handleJoinSession={this.handleJoinSession}
+                                isLoading={isLoading}
+                                mySessionId={mySessionId}
+                                handleChangeSessionId={this.handleChangeSessionId}
+                                // handleSetisLoading={this.handleSetisLoading}
+                            />
+                        }
+                    />
+                    <Route
+                        exact
+                        path="/session"
+                        element={
+                            <SessionPage
+                                session={this.state.session}
+                                mySessionId={mySessionId}
+                                myUserName={myUserName}
+                                audioEnabled={audioEnabled}
+                                handleMainVideoStream={this.handleMainVideoStream}
+                                subscribers={this.state.subscribers}
+                                publisher={this.state.publisher}
                                 leaveSession={this.leaveSession}
                                 toggleAudio={this.toggleAudio}
-                                audioEnabled={audioEnabled}
-                                userName={myUserName}
-                                onSessionJoin={this.handleSessionJoin}
                                 speakingUserName={this.state.speakingUserName}
                                 isLoading={isLoading}
+                                handleSessionJoin={this.handleSessionJoin}
                             />
-                        </div>
-
-                        <div id="video-container">
-                            {this.state.publisher !== undefined ? (
-                                <div>
-                                    <UserVideoComponent streamManager={this.state.publisher} />
-                                </div>
-                            ) : null}
-                            {this.state.subscribers.map((sub, i) => (
-                                <div>
-                                    <UserVideoComponent streamManager={sub} />
-                                </div>
-                            ))}
-                        </div>
-                        {isLoading && <LoadingBox roomNumb={mySessionId} />}
-                    </div>
-                ) : null}
-            </div>
+                        }
+                    />
+                </Routes>
+            </Router>
         );
     }
 
---
+++ client/src/components/Page/HomePage.js
@@ -0,0 +1,114 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./HomePage.css";
+
+function HomePage(props) {
+    const {
+        isLoading,
+        handleCreateSession,
+        handleChangeUserName,
+        myUserName,
+        handleJoinSession,
+        handleChangeSessionId,
+        // handleSetisLoading,
+    } = props;
+
+    const navigate = useNavigate();
+
+    const redirectToSessionPage = () => {
+        // handleSetisLoading(true);
+        handleCreateSession();
+        navigate("/session"); // SessionPage로 이동
+    };
+
+    const redirectToSessionPage2 = () => {
+        handleJoinSession((response) => {
+            if (response) {
+                // handleSetisLoading(true);
+                navigate("/session"); // SessionPage로 이동
+            }
+        });
+    };
+
+    return (
+        <div id="join">
+            <div className="big-circles" style={{ pointerEvents: "none" }}>
+                <div className="big-circle"></div>
+                <div className="big-circle"></div>
+                <div className="big-circle"></div>
+            </div>
+            <section id="home">
+                <div className="slide-wrapper">
+                    <div className="smallcircles" style={{ pointerEvents: "none" }}>
+                        <div className="small-circle"></div>
+                        <div className="small-circle"></div>
+                        <div className="small-circle"></div>
+                        <div className="small-circle"></div>
+                        <div className="small-circle"></div>
+                        <div className="small-circle"></div>
+                    </div>
+                    <div id="join-dialog" className="jumbotron vertical-center">
+                        <h1 className="logo"></h1>
+                        <form className="form-group name" onSubmit={redirectToSessionPage}>
+                            <p>
+                                <label> NAME </label>
+                                <input
+                                    className="form-control"
+                                    type="text"
+                                    id="userName"
+                                    value={myUserName}
+                                    onChange={handleChangeUserName}
+                                    required
+                                />
+                            </p>
+                        </form>
+                        <div id="join-dialog-content">
+                            <div className="first">
+                                <p className="create">
+                                    <label id="label-create"> NEW ROOM </label>
+                                    <input
+                                        onClick={redirectToSessionPage}
+                                        className="btn btn-lg btn-success create"
+                                        name="commit"
+                                        type="submit"
+                                        value="CREATE"
+                                    />
+                                </p>
+                            </div>
+                            <div>
+                                <form
+                                    className="form-group"
+                                    onSubmit={(event) => {
+                                        event.preventDefault();
+                                        redirectToSessionPage2();
+                                    }}
+                                >
+                                    <p>
+                                        <input
+                                            className="form-control"
+                                            type="text"
+                                            id="sessionId"
+                                            onChange={handleChangeSessionId}
+                                            style={{ pointerEvents: "auto" }}
+                                            placeholder="   # INVITE CODE"
+                                        />
+                                    </p>
+                                    <p className="text-center">
+                                        <input
+                                            className="btn btn-lg btn-success"
+                                            name="commit"
+                                            type="submit"
+                                            value="JOIN"
+                                        />
+                                    </p>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default HomePage;
